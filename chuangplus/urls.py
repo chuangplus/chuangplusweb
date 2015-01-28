bf84f54c1d064d796c551fefc6c892ca3c4c1bdf
charlotte <lingboxunmeng@163.com>
2015-01-29 00:07:49 +0800

    01-29-0:07 cly

--- chuangplus/urls.py
+++ chuangplus/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.conf import settings
 from app.views import *
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
