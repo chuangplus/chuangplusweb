7247f449c94a83d9dc0365aace077a455a34992e
yangxiaoFrank <yangxiao5201@163.com>
2015-01-28 21:37:52 +0800

    yx-1.28
    update databases

--- app/models.py
+++ app/models.py
@@ -5,7 +5,6 @@ from django.db import models
 
 # 用户信息表
 class userinfo(models.Model):
-	user_id = models.IntegerField()
 	role = models.IntegerField(default=1)
 	name = models.CharField(max_length=30)
 	gender = models.IntegerField(default=1)
@@ -21,7 +20,6 @@ class userinfo(models.Model):
 
 # 项目表
 class projects(models.Model):
-	pro_id = models.IntegerField()
 	name = models.CharField(max_length=30)
 	field1 = models.CharField(max_length=30)
 	field2 = models.CharField(max_length=30)
@@ -47,7 +45,6 @@ class projects(models.Model):
 	
 # 项目成员表
 class members(models.Model):
-	member_id = models.IntegerField()
 	pro_id = models.IntegerField()
 	m_name = models.CharField(max_length=30)
 	m_head_path = models.CharField(max_length=250)
@@ -56,7 +53,6 @@ class members(models.Model):
 	
 # 文章表（包括网站公告、平台新闻、大事记）
 class posts(models.Model):
-	post_id = models.IntegerField()
 	pro_id = models.IntegerField(default=0) # pro_id=-1:网站公告; pro_id=0:平台新闻; pro_id=projects.pro_id:项目大事记; 
 	date = models.DateField()
 	title = models.CharField(max_length=60)
@@ -66,7 +62,6 @@ class posts(models.Model):
 	
 # 关系表
 class relation(models.Model):
-	rel_id = models.IntegerField()
 	user_id = models.IntegerField()
 	pro_id = models.IntegerField()
 	date = models.DateField()
@@ -74,7 +69,6 @@ class relation(models.Model):
 	
 # 图片表
 class images(models.Model):
-	image_id = models.IntegerField()
 	pro_id = models.IntegerField() # pro_id=-1：首页滚动焦点图; pro_id=0:其他地方插图; pro_id=projects.pro_id:项目路演ppt图
 	image_path = models.CharField(max_length=250)
 	title = models.CharField(max_length=60)
