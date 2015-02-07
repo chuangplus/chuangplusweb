dcdf428abd1dbe90c0a4b7c4a3eb45a5fa976493
charlotte <lingboxunmeng@163.com>
2015-02-07 15:34:33 +0800

    Merge remote-tracking branch 'origin/master'
    Conflicts:
    	.idea/workspace.xml
    
    我回来啦 by cly

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,14 @@
+# coding: utf-8
 from django.contrib import admin
 
 # Register your models here.
+
+from app.models import *
+
+admin.site.register( userinfo )
+admin.site.register( project )
+admin.site.register( member )
+admin.site.register( post )
+admin.site.register( relation )
+admin.site.register( image )
+	
